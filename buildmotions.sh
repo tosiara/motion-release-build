d0c4e6e7475d563c7aa03934c388f67b07a2cedf
tosiara <tosiara@users.noreply.github.com>
2024-11-08 14:11:19 +0200

    Update buildmotions.sh

--- buildmotions.sh
+++ buildmotions.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 arch="amd64 i386 arm64v8 arm32v7 arm32v5"
+arch="arm64v8"
 
 platforms="ubuntu:xenial ubuntu:bionic ubuntu:focal ubuntu:jammy ubuntu:noble debian:stretch debian:buster debian:bullseye debian:bookworm debian:trixie"
+platforms="debian:bookworm"
 # Currently build script does not support i386/ubuntu:focal because of missing libmariadbclient-dev
 
 email="tosiara@users.noreply.github.com"
