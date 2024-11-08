ce12e77559eab8e33da30a9f29d3603704161a81
tosiara <tosiara@users.noreply.github.com>
2024-11-08 16:48:32 +0200

    Update buildmotions.sh

--- buildmotions.sh
+++ buildmotions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 arch="amd64 i386 arm64v8 arm32v7 arm32v5"
-arch="arm64v8 arm32v7"
+arch="arm32v7"
 
 platforms="ubuntu:xenial ubuntu:bionic ubuntu:focal ubuntu:jammy ubuntu:noble debian:stretch debian:buster debian:bullseye debian:bookworm debian:trixie"
-platforms="debian:bookworm ubuntu:jammy"
+platforms="ubuntu:jammy debian:bookworm"
 # Currently build script does not support i386/ubuntu:focal because of missing libmariadbclient-dev
 
 email="tosiara@users.noreply.github.com"
@@ -23,7 +23,8 @@ do
    		if [ "$a" == "arm32v7" ]; then d="linux/arm32/v7"; fi
      		if [ "$a" == "arm32v5" ]; then d="linux/arm32/v5"; fi
        		if [ "$a" == "i386" ];    then d="linux/386"; fi
-		image="registry.hub.docker.com/$a/$p"
+	 	if [ -z `docker run --rm mplatform/mquery | grep $p` ]; then continue; fi 
+		image="$a/$p"
 		docker run --platform "$d" -v "$LOCAL:/debs" --env "PLATFORM=$image" --env "VERSION=$v" --env "EMAIL=$email" "$image" /debs/entrypoint.sh
 	done
 done
