e1254bd18c80eb48dec085fdaeadbca1e8fdae61
tosiara <tosiara@users.noreply.github.com>
2024-11-08 16:24:47 +0200

    Update buildmotions.sh

--- buildmotions.sh
+++ buildmotions.sh
@@ -18,7 +18,12 @@ for p in $platforms
 do
 	for a in $arch
 	do
+ 		if [ "$a" == "amd64" ];   then d="$a"; fi
+ 		if [ "$a" == "arm64v8" ]; then d="linux/arm64/v8"; fi
+   		if [ "$a" == "arm32v7" ]; then d="linux/arm32/v7"; fi
+     		if [ "$a" == "arm32v5" ]; then d="linux/arm32/v5"; fi
+       		if [ "$a" == "i386" ];    then d="linux/386"; fi
 		image="registry.hub.docker.com/$a/$p"
-		docker run --platform linux/arm64/v8 -v "$LOCAL:/debs" --env "PLATFORM=$image" --env "VERSION=$v" --env "EMAIL=$email" "$image" /debs/entrypoint.sh
+		docker run --platform "$d" -v "$LOCAL:/debs" --env "PLATFORM=$image" --env "VERSION=$v" --env "EMAIL=$email" "$image" /debs/entrypoint.sh
 	done
 done
