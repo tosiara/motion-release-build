bb5660d6ea273e9e801598d19253a5aa29a3d159
tosiara <tosiara@users.noreply.github.com>
2024-11-11 14:30:43 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/basdh
+
+# otherwise you will get exec format error
+apt-get install -y binfmt-support qemu-user-static
+
+platforms="ubuntu:xenial ubuntu:bionic ubuntu:focal ubuntu:jammy ubuntu:noble debian:stretch debian:buster debian:bullseye debian:bookworm debian:trixie"
+# Currently build script does not support i386/ubuntu:focal because of missing libmariadbclient-dev
+
+for p in $platforms
+do
+    ./buildmotion.sh "$p"
+done
