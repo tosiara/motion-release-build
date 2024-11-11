c8e6fa88ce49aa06c0775e1c4bebf7719e879cd3
tosiara <tosiara@users.noreply.github.com>
2024-11-11 09:42:16 +0200

    Update buildmotions.sh

--- buildmotions.sh
+++ buildmotions.sh
@@ -24,10 +24,12 @@ do
    		if [ "$a" == "arm32v7" ]; then d="linux/arm32/v7"; fi
      		if [ "$a" == "arm32v5" ]; then d="linux/arm32/v5"; fi
        		if [ "$a" == "i386" ];    then d="linux/386"; fi
+	 	echo "Target image: $image"
+   		echo "Target docker platform: $d"
 	 	docker_platforms=`docker run --rm mplatform/mquery $image`
-   		echo $docker_platforms
-     		echo $d
-	 	if [ -z `echo $docker_platforms | grep $d` ]; then continue; fi 
+   		echo "docker platform: $docker_platforms"
+	 	if [ -z `echo $docker_platforms | grep $d` ]; then continue; fi
+   		echo "Building..."
 		docker run --platform "$d" -v "$LOCAL:/debs" --env "PLATFORM=$image" --env "VERSION=$v" --env "EMAIL=$email" "$image" /debs/entrypoint.sh
 	done
 done
