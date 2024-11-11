809ea270e561bd793d3f5ae95cb7384ef461b631
tosiara <tosiara@users.noreply.github.com>
2024-11-11 15:39:21 +0200

    Update build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,7 @@
 #!/bin/basdh
 
 # otherwise you will get exec format error
+apt-get update
 apt-get install -y binfmt-support qemu-user-static
 
 platforms="ubuntu:xenial ubuntu:bionic ubuntu:focal ubuntu:jammy ubuntu:noble debian:stretch debian:buster debian:bullseye debian:bookworm debian:trixie"
