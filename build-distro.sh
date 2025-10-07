b92923e7e8dbc6c29b4e1a7c8a63ce9e9f05545a
tosiara <tosiara@users.noreply.github.com>
2025-10-07 14:13:11 +0300

    Whitespace update

--- build-distro.sh
+++ build-distro.sh
@@ -11,17 +11,18 @@ do
 	image="$a/$1"
 	docker_platforms=`docker run --rm mplatform/mquery $image`
 	if [ "$a" == "amd64" ];   then is_supported=`echo $docker_platforms | grep amd64`; d="$a"; fi
- 	if [ "$a" == "arm64v8" ]; then is_supported=`echo $docker_platforms | grep arm64`; d="arm64/v8"; fi
-   	if [ "$a" == "arm32v7" ]; then is_supported=`echo $docker_platforms | grep -E 'linux\/arm$|linux\/arm\/v7'`; d="arm/v7"; fi
-     	if [ "$a" == "arm32v5" ]; then is_supported=`echo $docker_platforms | grep -E 'linux\/arm$|linux\/arm\/v5'`; d="arm/v5"; fi
-       	if [ "$a" == "i386" ];    then is_supported=`echo $docker_platforms | grep 386`; d="386"; fi
+	if [ "$a" == "arm64v8" ]; then is_supported=`echo $docker_platforms | grep arm64`; d="arm64/v8"; fi
+	if [ "$a" == "arm32v7" ]; then is_supported=`echo $docker_platforms | grep -E 'linux\/arm$|linux\/arm\/v7'`; d="arm/v7"; fi
+	if [ "$a" == "arm32v5" ]; then is_supported=`echo $docker_platforms | grep -E 'linux\/arm$|linux\/arm\/v5'`; d="arm/v5"; fi
+	if [ "$a" == "i386" ];    then is_supported=`echo $docker_platforms | grep 386`; d="386"; fi
 	 
 	echo "Target image: $image"
-   	echo "Target docker platform: $d"
-   	echo "docker platform: $docker_platforms"
-       	echo "Is supported: $is_supported"
+	echo "Target docker platform: $d"
+	echo "docker platform: $docker_platforms"
+	echo "Is supported: $is_supported"
 
 	if [ -z "$is_supported" ]; then continue; fi
-   	echo "Building..."
+	echo "Building..."
 	docker run --platform "linux/$d" -v "$LOCAL:/debs" --env "PLATFORM=$image" --env "VERSION=$v" --env "EMAIL=$email" "$image" /debs/entrypoint.sh
+
 done
